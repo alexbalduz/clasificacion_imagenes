0fc1b267f333b0f0dbe4a6e7e48f1015dd53d092
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-12-20 12:22:37 +0100

    intreprete no importa bien, problemas herencia

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import convolucion_capas.1_capa_convolucion as 1_capa_convolucion
 import convolucion_capas.1_capa_convolucion_aumentada as 1_capa_convolucion_aumentada
 import convolucion_capas.4_capas_convolucion_aumentada as 4_capas_convolucion_aumentada
+import convolucion_capas.clasificacion as clasificacion
+
+
 
 if __name__ == "__main__":
     1_capa_convolucion.main()
     1_capa_convolucion_aumentada.main()
-    4_capas_convolucion_aumentada.main()
+    4_convolucion_capas_aumentada.main()
+    clasificacion.main()
